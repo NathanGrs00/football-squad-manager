ba5399daa957892c7f56a8ecb100e0d41f4c8423
NathanGRS00 <nathangeers00@gmail.com>
2025-01-27 13:05:41 +0100

    Feat: Position and Player classes

---
+++ src/main/java/com/nathan/footballsquadmanagerbp2/models/Player.java
@@ -0,0 +1,67 @@
+package com.nathan.footballsquadmanagerbp2.models;
+
+public class Player {
+    private int playerId;
+    private String playerFirstName;
+    private String playerLastName;
+    private int playerAge;
+    private String playerPrefFoot;
+    private String playerStatus;
+
+    public Player(int playerId, String playerFirstName, String playerLastName, int playerAge, String playerPrefFoot, String playerStatus) {
+        this.playerId = playerId;
+        this.playerFirstName = playerFirstName;
+        this.playerLastName = playerLastName;
+        this.playerAge = playerAge;
+        this.playerPrefFoot = playerPrefFoot;
+        this.playerStatus = playerStatus;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public String getPlayerFirstName() {
+        return playerFirstName;
+    }
+
+    public String getPlayerLastName() {
+        return playerLastName;
+    }
+
+    public int getPlayerAge() {
+        return playerAge;
+    }
+
+    public String getPlayerPrefFoot() {
+        return playerPrefFoot;
+    }
+
+    public String getPlayerStatus() {
+        return playerStatus;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public void setPlayerFirstName(String playerFirstName) {
+        this.playerFirstName = playerFirstName;
+    }
+
+    public void setPlayerLastName(String playerLastName) {
+        this.playerLastName = playerLastName;
+    }
+
+    public void setPlayerAge(int playerAge) {
+        this.playerAge = playerAge;
+    }
+
+    public void setPlayerPrefFoot(String playerPrefFoot) {
+        this.playerPrefFoot = playerPrefFoot;
+    }
+
+    public void setPlayerStatus(String playerStatus) {
+        this.playerStatus = playerStatus;
+    }
+}
---
+++ src/main/java/com/nathan/footballsquadmanagerbp2/models/Position.java
@@ -0,0 +1,37 @@
+package com.nathan.footballsquadmanagerbp2.models;
+
+public class Position {
+    private int positionId;
+    private String positionName;
+    private String positionAbreviation;
+
+    public Position(int positionId, String positionName, String positionAbreviation) {
+        this.positionId = positionId;
+        this.positionName = positionName;
+        this.positionAbreviation = positionAbreviation;
+    }
+
+    public int getPositionId() {
+        return positionId;
+    }
+
+    public String getPositionName() {
+        return positionName;
+    }
+
+    public String getPositionAbreviation() {
+        return positionAbreviation;
+    }
+
+    public void setPositionId(int positionId) {
+        this.positionId = positionId;
+    }
+
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
+
+    public void setPositionAbreviation(String positionAbreviation) {
+        this.positionAbreviation = positionAbreviation;
+    }
+}
