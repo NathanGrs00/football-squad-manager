5fc847dd9ca5d4767c62045a81c407bb49dd636d
NathanGRSDesktop <nathangeers00@gmail.com>
2025-02-20 14:01:13 +0100

    Refactor: larger Button because of the image in PlayerDetailsView

--- src/main/java/com/nathan/footballsquadmanagerbp2/view/PlayerDetailsView.java
+++ src/main/java/com/nathan/footballsquadmanagerbp2/view/PlayerDetailsView.java
@@ -205,7 +205,7 @@ public class PlayerDetailsView {
     }
 
     public Scene getScene() {
-        Scene homeScene = new Scene(root, 650, 700);
+        Scene homeScene = new Scene(root, 700, 700);
         homeScene.getStylesheets().add("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
         homeScene.getStylesheets().add(getClass().getResource("/stylesheets/playerdetails-stylesheet.css").toExternalForm());
         return homeScene;
